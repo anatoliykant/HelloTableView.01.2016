1d17566c2b9bee6573c108b0e7b80621040f605e
anatoliykant <kant.anatoliy@yandex.ru>
2016-01-25 19:23:44 +0300

    Initial Commit

---
+++ HelloTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloTableView
+//
+//  Created by anatoliy.kant on 25.01.16.
+//  Copyright © 2016 anatoliy.kant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
